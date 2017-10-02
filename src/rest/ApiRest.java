fc9a41b4b3e50043a74ee8917ff06cb9af61ea43
ibmclouddemo <32460924+ibmclouddemo@users.noreply.github.com>
2017-10-02 12:12:15 -0400

    Update ApiRest.java

--- src/rest/ApiRest.java
+++ src/rest/ApiRest.java
@@ -28,8 +28,8 @@ public class ApiRest extends javax.ws.rs.core.Application {
     @Produces("application/json")
     @Path ("data/sales_expenses")
     public Response getJson() {
-        String noDB = System.getenv("DATABASE_CONNECTION_USE_SAMPLE");
-        if (noDB != null && noDB.equals("yes")) {
+        String connectionURL = System.getenv("DATABASE_CONNECTION_URL");
+        if (connectionURL == null) {
             
             String sample = "{\"data\":\"[['Month', 'Sales', 'Expenses'],['01',0.0,0.0],['02',0.0,940.0],['03',200.0,0.0],['04',0.0,30.0],['05',0.0,0.0],['06',1000.0,70.0],['07',0.0,0.0],['08',200.0,10.0],['09',0.0,250.0],['10',600.0,0.0],['11',0.0,0.0],['12',400.0,0.0]]\"}";
             return Response.ok(sample, MediaType.APPLICATION_JSON).build();
@@ -50,8 +50,8 @@ public class ApiRest extends javax.ws.rs.core.Application {
     @Path ("data/sales_by_country")
     public Response getSalesByCountry() {
         
-        String noDB = System.getenv("DATABASE_CONNECTION_USE_SAMPLE");
-        if (noDB != null && noDB.equals("yes")) {
+        String connectionURL = System.getenv("DATABASE_CONNECTION_URL");
+        if (connectionURL == null) {
             String sample = "{\"data\":\"[['Country', 'Total Sale'],['France',1600.0],['USA',400.0],['UK',400.0]]\"}";
             return Response.ok(sample, MediaType.APPLICATION_JSON).build();
         }
@@ -70,8 +70,8 @@ public class ApiRest extends javax.ws.rs.core.Application {
     @Path ("data/sales_by_product")
     public Response getSalesByProduct() {
         
-        String noDB = System.getenv("DATABASE_CONNECTION_USE_SAMPLE");
-        if (noDB != null && noDB.equals("yes")) {
+        String connectionURL = System.getenv("DATABASE_CONNECTION_URL");
+        if (connectionURL == null) {
             String sample = "{\"data\":\"[['Product', 'Total Sale'],['Cessna 152',400.0],['Cessna 172',1000.0],['Cessna 182',1000.0],]\"}";
             return Response.ok(sample, MediaType.APPLICATION_JSON).build();
         }
@@ -226,6 +226,15 @@ public class ApiRest extends javax.ws.rs.core.Application {
         }
 
         return Response.ok(version.toString(), MediaType.APPLICATION_JSON).build();
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path ("dbtest")
+    public Response getDBTest() {
+        
+        String connectionURL = System.getenv("DATABASE_CONNECTION_URL");
+        return Response.ok(connectionURL, MediaType.APPLICATION_JSON).build();
     }
 
 }
